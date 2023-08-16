e034761fcc5123e6c3255509de947d7b0a80e8ab
Maqsudjon <116064133+maqsudj0n1@users.noreply.github.com>
2023-08-16 22:22:53 +0500

    Add project files.

---
+++ Reflection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Product product = new Product()
+            {
+                Name = "AAA",
+                Description = "BBB"
+            };
+            Print(product);
+        }
+        static void Print(dynamic obj)
+        {
+            PropertyInfo[] properties = obj.GetType().GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine(property.GetValue(obj));
+            }
+        }
+    }
+}
